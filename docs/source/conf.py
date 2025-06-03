63ada65ddc16736ebb0cdc21de168226fbe163f9
Danilo Marto de Carvalho <opropriodanilomarto@gmail.com>
2025-06-02 23:10:53 -0300

    chore(docs): adicionar Sphinx para documentação
    Sphinx foi adicionado para gerar documentação técnica do projeto.
    Configurações iniciais e estrutura base foram criadas.

---
+++ docs/source/conf.py
@@ -0,0 +1,40 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+import django
+
+
+project = 'DjangoSIGE'
+copyright = '2025, Thiago Pena'
+author = 'Thiago Pena'
+release = 'v1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'pt_BR'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+sys.path.insert(0, os.path.abspath('../../'))  # caminho até o manage.py
+os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings'  # ajuste para o nome correto
+
+django.setup()
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. DjangoSIGE documentation master file, created by
+   sphinx-quickstart on Mon Jun  2 23:03:13 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to DjangoSIGE's documentation!
+======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
