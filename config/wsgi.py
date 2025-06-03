89862c25057b3ecedc9a66beeaf5cc95eb2f6667
Danilo Marto de Carvalho <opropriodanilomarto@gmail.com>
2025-06-02 21:06:27 -0300

    refactor(infra): reescrever todos os módulos do pacote config/

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,19 +1,6 @@
-"""
-WSGI config for djangosige project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
-"""
+"""Configurações do WSGI para o projeoto DjangoSIGE."""
 
 import os
-import sys
-
-sys.path.insert(0, os.path.abspath(
-    os.path.join(os.path.dirname(__file__), "../../")))
-sys.path.insert(0, os.path.abspath(
-    os.path.join(os.path.dirname(__file__), "../")))
 
 from django.core.wsgi import get_wsgi_application
 
