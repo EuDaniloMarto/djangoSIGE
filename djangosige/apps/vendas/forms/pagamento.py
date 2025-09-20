56981e1354f166a1c9233e0fd70f12ca020ab68b
Danilo Marto de Carvalho <eudanilomarto@gmail.com>
2025-09-20 07:23:31 -0300

    :broom: substituir a função descontinuada django.utils.translation.ugettext_lazy

--- djangosige/apps/vendas/forms/pagamento.py
+++ djangosige/apps/vendas/forms/pagamento.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from django import forms
-from django.utils.translation import ugettext_lazy as _
 from django.forms import inlineformset_factory
+from django.utils.translation import gettext_lazy as _
 
-from djangosige.apps.vendas.models import Venda, Pagamento, CondicaoPagamento
+from djangosige.apps.vendas.models import CondicaoPagamento, Pagamento, Venda
 
 
 class PagamentoForm(forms.ModelForm):
