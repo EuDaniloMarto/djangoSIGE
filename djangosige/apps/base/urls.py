b8a706313c70fa1acd220f0947c3900342a9c756
Danilo Marto de Carvalho <dmcarvalho@tutamail.com>
2025-10-18 22:27:54 -0300

    fix: substituir função de rotiamento por regex

--- djangosige/apps/base/urls.py
+++ djangosige/apps/base/urls.py
@@ -1,17 +1,15 @@
-# -*- coding: utf-8 -*-
+from django.conf import settings
+from django.urls import re_path as url
 
-from django.conf.urls import url
 from . import views
 
-from djangosige.configs import DEBUG
-
-app_name = 'base'
+app_name = "base"
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r"^$", views.IndexView.as_view(), name="index"),
 ]
 
-if DEBUG:
+if settings.DEBUG:
     urlpatterns += [
-        url(r'^404/$', views.handler404),
-        url(r'^500/$', views.handler500),
+        url(r"^404/$", views.handler404),
+        url(r"^500/$", views.handler500),
     ]
