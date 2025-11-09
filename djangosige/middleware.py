90f7c542f8393762ba29ac35cf5f0da7996498a7
Danilo Marto de Carvalho <dmcarvalho@tutamail.com>
2025-11-09 04:12:58 -0300

    fix(apps): corrigir erros de importação gerados pela refatoração do módulo config

--- djangosige/middleware.py
+++ djangosige/middleware.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 
 import re
+
+from django.conf import settings
 from django.shortcuts import redirect
 from django.utils.deprecation import MiddlewareMixin
-
-from .configs.settings import LOGIN_NOT_REQUIRED
 
 
 class LoginRequiredMiddleware(MiddlewareMixin):
 
     def __init__(self, get_response=None, *args, **kwargs):
-        self.exceptions = tuple(re.compile(url) for url in LOGIN_NOT_REQUIRED)
+        self.exceptions = tuple(re.compile(url) for url in settings.LOGIN_NOT_REQUIRED)
         self.get_response = get_response
 
         return super(LoginRequiredMiddleware, self).__init__(get_response, *args, **kwargs)
@@ -20,11 +20,11 @@ class LoginRequiredMiddleware(MiddlewareMixin):
         if request.user.is_authenticated:
             for url in self.exceptions:
                 if url.match(request.path):
-                    return redirect('base:index')
+                    return redirect("base:index")
             return None
 
         for url in self.exceptions:
             if url.match(request.path):
                 return None
 
-        return redirect('login:loginview')
+        return redirect("login:loginview")
